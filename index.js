3323ee59215f6336ee4a5a340e0f96cb7bffc30f
joaocastro <joaocastro0429@gmail.com>
2024-04-05 23:21:38 -0300

    add middleware

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ const port=3000
 const path=require('path')
 
 const app=express()
+// middleware
+function checkAuth(request,response,next){
+
+    request.authStatus=true
+
+    if(request.authStatus){
+        console.log("esta logando, faça o login para continuar")
+    }else{
+        console.log("não esta logado , faça o login para continuar")
+    }
+    next()
+
+}
+app.use(checkAuth)
 
 app.get("/",(request,response)=>{
 
